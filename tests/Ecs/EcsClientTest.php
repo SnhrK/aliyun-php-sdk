2a62095629c78e10832e0128d9afb01518cd5851
SnhrK <suna@sunaharakenyuunoMacBook-Pro.local>
2017-08-24 15:15:04 +0900

    modify testfunctions

--- tests/Ecs/EcsClientTest.php
+++ tests/Ecs/EcsClientTest.php
@@ -6,26 +6,24 @@ use Aliyun\Ecs\EcsClient;
 class EcsClientTest extends AliyunTestBase {
     /**
      * @override
-     * @return Mobingi\Core\User\Action
+     * @return Aliyun\Ecs\EcsClient
      */
     protected function getTargetInstance() {
         return new EcsClient();
     }
 
     function testSetClient() {
-        $this->target->setClient(self::TEST_REGION, $_SERVER['TEST_ALIYUN_ACCESS'], $_SERVER['TEST_ALIYUN_SECRET']);
+        $this->setEcsClient();
         $actual = (array)$this->target->aliyunclient;
         $this->assertArrayHasKey("iClientProfile", $actual);
         $this->assertArrayHasKey("__urlTestFlag__", $actual);
     }
 
-   /**
-   * Test for testDescribeRegion
-   */
-    public function testDescribeRegion() {
-        $this->setEcsClient();
-        $setter = [];
-        $actual = $this->target->describeRegion($setter);
+    /**
+    * Test for testDescribeRegion
+    */
+    public function testDescribeRegion($setter = []) {
+        $actual = $this->setEcsClient()->target->describeRegion($setter);
         $this->assertInternalType("array", $actual);
         $this->assertArrayHasKey("RequestId", $actual);
         $this->assertArrayHasKey("Regions", $actual);
@@ -34,56 +32,50 @@ class EcsClientTest extends AliyunTestBase {
     /**
     * Test for testCreateVpc
     */
-     public function testCreateVpc() {
-         $this->setEcsClient();
-         $setter = ['CidrBlock' => '10.0.0.0/08', 'VpcName' => 'aliyun-php-test'];
-         $actual = $this->target->createVpc($setter);
-         $this->assertInternalType("array", $actual);
-         $this->assertArrayHasKey("VpcId", $actual);
-     }
+    public function testCreateVpc() {
+        $setter = ['CidrBlock' => '10.0.0.0/08', 'VpcName' => 'aliyun-php-test'];
+        $actual = $this->setEcsClient()->target->createVpc($setter);
+        $this->assertInternalType("array", $actual);
+        $this->assertArrayHasKey("VpcId", $actual);
+    }
 
-     /**
-     * Test for testCreateVSwitch
-     */
-      public function testCreateVSwitch() {
-          $this->setEcsClient();
-          $setter = [];
-          $result = $this->target->describeVpc($setter);
-          $this->assertInternalType("array", $result);
-          $this->assertArrayHasKey("Vpcs", $result);
-          $setter = ['CidrBlock' => '10.0.0.0/24', 'VpcId' => $result['Vpcs']['Vpc'][0]['VpcId'], 'ZoneId' => self::TEST_ZONE];
-          $actual = $this->target->createVSwitch($setter);
-          $this->assertInternalType("array", $actual);
-          $this->assertArrayHasKey("VSwitchId", $actual);
-      }
+    /**
+    * Test for testCreateVSwitch
+    */
+    public function testCreateVSwitch($setter = []) {
+        $result = $this->setEcsClient()->target->describeVpc($setter);
+        $this->assertInternalType("array", $result);
+        $this->assertArrayHasKey("Vpcs", $result);
+        $setter = ['CidrBlock' => '10.0.0.0/24', 'VpcId' => $result['Vpcs']['Vpc'][0]['VpcId'], 'ZoneId' => self::TEST_ZONE];
+        $actual = $this->target->createVSwitch($setter);
+        $this->assertInternalType("array", $actual);
+        $this->assertArrayHasKey("VSwitchId", $actual);
+    }
 
-     /**
-     * Test for testDeleteVpc
-     */
-      public function testDeleteVpc() {
-          $this->setEcsClient();
-          $setter = [];
+    /**
+    * Test for testDeleteVpc
+    */
+    public function testDeleteVpc($setter = []) {
+        //delete vswitch
+        $result = $this->setEcsClient()->target->describeVSwitch($setter);
+        $this->assertInternalType("array", $result);
+        $this->assertArrayHasKey("VSwitches", $result);
+        $vswitch_setter = ['VSwitchId' => $result['VSwitches']['VSwitch'][0]['VSwitchId']];
+        $actual = $this->target->deleteVSwitch($vswitch_setter);
+        $this->assertInternalType("array", $actual);
 
-          //delete vswitch
-          $result = $this->target->describeVSwitch($setter);
-          $this->assertInternalType("array", $result);
-          $this->assertArrayHasKey("VSwitches", $result);
-          $vswitch_setter = ['VSwitchId' => $result['VSwitches']['VSwitch'][0]['VSwitchId']];
-          $actual = $this->target->deleteVSwitch($vswitch_setter);
-          $this->assertInternalType("array", $actual);
+        // //delete vpc
+        $result = $this->target->describeVpc($setter);
+        $this->assertInternalType("array", $result);
+        $this->assertArrayHasKey("Vpcs", $result);
+        $setter = ['VpcId' => $result['Vpcs']['Vpc'][0]['VpcId']];
+        $actual = $this->target->deleteVpc($setter);
+        $this->assertInternalType("array", $actual);
+    }
 
-          // //delete vpc
-          $result = $this->target->describeVpc($setter);
-          $this->assertInternalType("array", $result);
-          $this->assertArrayHasKey("Vpcs", $result);
-          $setter = ['VpcId' => $result['Vpcs']['Vpc'][0]['VpcId']];
-          $actual = $this->target->deleteVpc($setter);
-          $this->assertInternalType("array", $actual);
-      }
-
-      function setEcsClient() {
-          $this->target->setClient(self::TEST_REGION, $_SERVER['TEST_ALIYUN_ACCESS'], $_SERVER['TEST_ALIYUN_SECRET']);
-          return $this;
-      }
+    function setEcsClient() {
+        $this->target->setClient(self::TEST_REGION, $_SERVER['TEST_ALIYUN_ACCESS'], $_SERVER['TEST_ALIYUN_SECRET']);
+        return $this;
+    }
 
 }
